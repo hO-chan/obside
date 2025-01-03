71176d76eda880075368f3a54172887c3a72ce43
hO-chan <hwang151776@gmail.com>
2025-01-03 23:33:14 +0900

    기본적인 틀 구현중

--- obside/src/App.js
+++ obside/src/App.js
@@ -1,23 +1,36 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//여기서는 html과 유사한 jsx문법을 사용해서 해야한다.
 function App() {
+
+
+  let data1 = 'dt2';
+  let [data2, data2Change] = useState('대충 아무 데이터');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header" style={{ border: '1px solid black' }}>
+        NavBar
+      </div>
+
+      <div className='body' style={{ border: '1px solid black' }}>
+        <div className='bdyLBar'>
+          leftSideBar
+        </div>
+        <div className='bdyContent'>
+          <input></input>
+
+        </div>
+        <div className='bdyRBar'>
+          rightSideBar
+        </div>
+      </div>
+
+      <div className='footer' style={{ border: '1px solid black' }}>
+        footer
+      </div>
     </div>
   );
 }
